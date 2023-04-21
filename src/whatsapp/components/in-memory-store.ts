374db4b26112c33d24404a53fdecb502a05d9b0f
Matheus Deister Veiga <matheus.veiga@mundiale.com.br>
2023-04-21 19:06:19 -0300

    chore: point dependency to new repo

--- src/whatsapp/components/in-memory-store.ts
+++ src/whatsapp/components/in-memory-store.ts
@@ -8,7 +8,9 @@ const ROOT_PATH = getRoot()
 const STORE_PATH = path.join(ROOT_PATH, 'baileys_store.json')
 
 const store = makeInMemoryStore({ logger })
+
 store.readFromFile(STORE_PATH)
+
 setInterval(() => {
     store.writeToFile(STORE_PATH)
 }, 10_000)
